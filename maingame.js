f3f9f7cd8d3ac6894dba56783c26aa93f79dbb7d
DouglasHeath <foundations.douglas@gmail.com>
2017-05-02 19:49:21 -0700

    I'm an idiot

--- maingame.js
+++ maingame.js
@@ -1,3 +1,4 @@
+$(document).ready() {
 // Initialize the game map
 var gameMap = [];
 
@@ -583,3 +584,5 @@ resetButton.addEventListener("click", resetGame, false);
 // Closing help box
 var closeHelp = document.getElementById("closeHelp");
 closeHelp.addEventListener("click", closeHelpButton, false);
+
+};
